33378ff829deda4baee92bea78c63c851c88575a
1zLamar <88988558+1zLamar@users.noreply.github.com>
2023-05-31 01:13:12 +0300

    Delete Day21-excersice directory

--- Day21-excersice/src/main/java/com/example/day21excersice/ApiException/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.day21excersice.ApiException;
-
-public class ApiException extends RuntimeException{
-
-    public ApiException(String msg){
-        super(msg);
-    }
-}
--- Day21-excersice/src/main/java/com/example/day21excersice/ApiResponse/ApiResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.day21excersice.ApiResponse;
-
-public class ApiResponse {
-
-    private String msg;
-
-}
